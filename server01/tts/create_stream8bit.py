0f4b1b71ebf0f167a020a922219f42ee5104fc21
PedroArtDesk <pedroarteta@hotmail.es>
2025-06-24 15:43:09 -0500

    modelo de 8bit

---
+++ server01/tts/create_stream8bit.py
@@ -0,0 +1,16 @@
+import asyncio
+import sounddevice as sd
+from kokoro_onnx import Kokoro
+
+async def main():
+    kokoro = Kokoro("kokoro-v1.0.int8.onnx", "voices-v1.0.bin")
+
+    text = "¡Hola! Esto se genera en tiempo real con Kokoro‑onnx."
+    stream = kokoro.create_stream(text, voice="af_sarah", speed=1.0, lang="es")
+
+    async for samples, sample_rate in stream:
+        sd.play(samples, sample_rate)
+        sd.wait()
+
+if __name__ == "__main__":
+    asyncio.run(main())
